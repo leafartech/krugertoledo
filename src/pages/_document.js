d48c29e45dc8e31b1b7235b129cd293acebe47f7
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-26 13:39:33 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,6 +19,12 @@ export default function Document() {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-NLM24BX');
+              (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "j1a2usrg69");
+
             `,
           }}
         />
